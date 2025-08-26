32293e8ae17edaed3c95255ed3ae370d672c9487
MinerovyLP <152312938+MinerovyLP@users.noreply.github.com>
2025-08-26 22:54:37 +0330

    test4

--- src/main/java/dev/lvstrng/argon/font/GlyphPageFontRenderer.java
+++ src/main/java/dev/lvstrng/argon/font/GlyphPageFontRenderer.java
@@ -248,18 +248,13 @@ public final class GlyphPageFontRenderer {
 		return i;
 	}
 
-    public int drawString(MatrixStack matrices, CharSequence text, float x, float y, int z, int color, boolean dropShadow) {
+    public int drawString(MatrixStack matrices, CharSequence text, float x, float y, int z, int color) {
 		this.resetStyles();
 		int i;
 
         matrices.translate(0, 0, z);
 
-		if (dropShadow) {
-			i = this.renderString(matrices, text, x + 1.0F, y + 1.0F, color, true);
-			i = Math.max(i, this.renderString(matrices, text, x, y, color, false));
-		} else {
-			i = this.renderString(matrices, text, x, y, color, false);
-		}
+		i = this.renderString(matrices, text, x, y, color, false);
 
 		return i;
 	}
