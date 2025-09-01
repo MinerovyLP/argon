65ef31e87744f0350769760bb643115449053fb7
MinerovyLP <152312938+MinerovyLP@users.noreply.github.com>
2025-09-01 04:16:47 +0330

    test

--- src/main/java/dev/lvstrng/argon/module/ModuleManager.java
+++ src/main/java/dev/lvstrng/argon/module/ModuleManager.java
@@ -50,7 +50,6 @@ public final class ModuleManager implements ButtonListener {
 		add(new NoJumpDelay());
 		add(new PingSpoof());
 		add(new FakeLag());
-        add(new FakePositionLag());
 		add(new AutoClicker());
 		add(new KeyPearl());
 		add(new NoBreakDelay());
