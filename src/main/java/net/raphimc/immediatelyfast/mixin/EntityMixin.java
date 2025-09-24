aaa0ff3cb9954aca286c79d282c781d8dd63339e
MinerovyLP <152312938+MinerovyLP@users.noreply.github.com>
2025-09-24 17:55:59 +0330

    Update EntityMixin.java

--- src/main/java/net/raphimc/immediatelyfast/mixin/EntityMixin.java
+++ src/main/java/net/raphimc/immediatelyfast/mixin/EntityMixin.java
@@ -1,17 +1,22 @@
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
-@ModifyExpressionValue(
-    method = "move",
-    at = @At(
-        value = "INVOKE",
-        target = "Lnet/minecraft/entity/Entity;isControlledByPlayer()Z"
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @ModifyExpressionValue(
+        method = "move",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;isControlledByPlayer()Z"
+        )
     )
-)
-private boolean fixFallDistance(boolean original) {
-    if ((Object) this == MinecraftClient.getInstance().player) {
-        return false;
+    private boolean fixFallDistance(boolean original) {
+        if ((Object) this == MinecraftClient.getInstance().player) {
+            return false;
+        }
+        return original;
     }
-    return original;
 }
